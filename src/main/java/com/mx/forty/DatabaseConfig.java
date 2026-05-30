8f8035bcd70329b20accf352899c2774c210e113
oscarglzmoreno13-bit <oscarglzmoreno.13@gmail.com>
2026-05-30 14:51:16 -0600

    actualizacion

--- src/main/java/com/mx/forty/DatabaseConfig.java
+++ src/main/java/com/mx/forty/DatabaseConfig.java
@@ -20,7 +20,7 @@ public class DatabaseConfig {
 	    public DataSource dataSource() {
 	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 	        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	        dataSource.setUrl("jdbc:mysql://localhost:3306/forty_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC");
+	        dataSource.setUrl("jdbc:mysql://dadtabse-forty.cv6m4gewkqci.us-east-2.rds.amazonaws.com");
 	        dataSource.setUsername("FtyUsr");
 	        dataSource.setPassword("Fg120615@");
 	        return dataSource;
